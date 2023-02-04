d4082f04b887b091afdbf3af9ed0ad31734683ac
dmira1 <darryl.pmira@gmail.com>
2023-02-04 10:50:03 -0700

    Navbar Changes + Media Query changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-function toggleMenu(x) {
-  document.querySelector('body').classList.toggle("change");
-}
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+const navList = document.getElementById("navList");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle("change");
+  nav.classList.toggle("change");
+  navList.classList.toggle("change");
+});
